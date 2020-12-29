d19127c273a7b2624208795b8e970d480a36f23e
ipe <ian.ensor-cic-uk@ibm.com>
2020-12-29 17:05:53 +0000

    bin off designs page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/views/Index.vue'
-import Designs from '@/views/Designs'
 import MainNavbar from './layout/MainNavbar.vue'
 import MainFooter from './layout/MainFooter.vue'
 
@@ -14,15 +13,6 @@ export default new Router({
       path: '/',
       name: 'index',
       components: { default: Index, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: 'black' }
-      }
-    },
-    {
-      path: '/designs',
-      name: 'designs',
-      components: { default: Designs, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: 'black' }
