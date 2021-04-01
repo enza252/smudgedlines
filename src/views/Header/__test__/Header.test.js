9e7face8dca6148b83a935c941001515db522eb0
ipe <ian.ensor-cic-uk@ibm.com>
2021-04-01 16:05:11 +0100

    render then test

--- src/views/Header/__test__/Header.test.js
+++ src/views/Header/__test__/Header.test.js
@@ -6,10 +6,16 @@ const options = {
   routes: [],
   vuetify: new Vuetify()
 }
+
+let fragment
+
+beforeAll(async () => {
+  const { asFragment } = await render(Header, options)
+  fragment = asFragment
+})
 describe('Tests the Header view is rendered correctly', () => {
-  it('Renders the Header and checks against a snapshot', async () => {
-    const { asFragment } = await render(Header, options)
-    expect(asFragment).toMatchSnapshot()
+  it('Renders the Header and checks against a snapshot', () => {
+    expect(fragment).toMatchSnapshot()
   })
   it('tests the nav bar button texts are present on the page', async () => {
     await render(Header, options)
