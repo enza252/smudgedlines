ad845785a3a2f43009f2bab60df3eb2f4a83e16c
ipe <ian.ensor-cic-uk@ibm.com>
2021-04-02 14:46:04 +0100

    add designs, begin playing with negative margin to get the sheet\/paper to overlap

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,8 +3,13 @@
     <v-row class="d-flex flex-grow-1" justify="center" align="center">
       <Header/>
     </v-row>
-    <v-row class="d-flex flex-column flex-grow-0">
+    <v-row class="d-flex flex-column flex-grow-1">
       <Splash/>
+    </v-row>
+    <v-row class="d-flex flex-column flex-grow-1" justify="center" align="center">
+      <v-sheet min-width="80%" rounded elevation="2" class="main" style="margin-top: -60px">
+        <Designs/>
+      </v-sheet>
     </v-row>
   </v-container>
 </template>
@@ -12,10 +17,12 @@
 <script>
 import Header from '@/views/Header/Header'
 import Splash from '@/views/Splash/Splash'
+import Designs from '@/views/Designs/Designs'
 
 export default {
   name: 'Index',
   components: {
+    Designs,
     Header,
     Splash
   }
---
+++ src/views/Designs/Designs.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h2>Designs</h2>
+    <v-card>
+      <v-card-title>Djibou</v-card-title>
+      <v-card-text>Zig/zag pattern in orange</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'Designs'
+}
+</script>
