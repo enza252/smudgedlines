c10a4d8a8c4cd0c6079530118997c3726019dd9e
ipe <ian.ensor-cic-uk@ibm.com>
2021-04-11 22:18:00 +0100

    t shirt icon

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+{
+  "env": {
+    "test": {
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            "useBuiltIns": "usage",
+            "corejs": "3.8.3",
+            "targets": { "node": "current" }
+          }
+        ]
+      ]
+    }
+  },
+  "plugins": [
+    "@babel/plugin-syntax-dynamic-import"
+  ]
+}
