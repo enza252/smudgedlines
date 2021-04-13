1d0292eb50947f694323ea773cc19a75d70b7935
ipe <ian.ensor-cic-uk@ibm.com>
2021-04-13 20:00:45 +0100

    wrap designs on a sheet so it looks nicer

--- components/designs.vue
+++ components/designs.vue
@@ -1,49 +1,53 @@
 <template>
   <v-container class="pa-6">
-    <v-row>
-      <h1 id="designs-header">Designs</h1>
-    </v-row>
-    <v-row>
-      <h4 id="designs-subheader">View our designs and Tees</h4>
-    </v-row>
-    <v-row>
-      <v-tabs
-        background-color="primary"
-        color="light"
-        icons-and-text
-        fixed-tabs
-        next-icon="mdi-arrow-right-bold-box-outline"
-        prev-icon="mdi-arrow-left-bold-box-outline"
-        show-arrows
-        v-model="tab"
-      >
-        <template v-for="(design, index) in tabHeaders">
-          <v-tab :key="index">{{ design.name }}
-            <v-icon :key="index">{{ design.icon }}</v-icon>
-          </v-tab>
-        </template>
-      </v-tabs>
-      <v-tabs-items v-model="tab">
-        <v-tab-item
-          v-for="(design, index) in designs"
-          :key="index"
+    <v-sheet color="accent" style="padding: 2em" rounded elevation="10">
+
+      <v-row>
+        <h1 id="designs-header">Designs</h1>
+      </v-row>
+      <v-row>
+        <h4 id="designs-subheader">View our designs and Tees</h4>
+      </v-row>
+      <v-row>
+        <v-tabs
+          background-color="primary"
+          color="light"
+          icons-and-text
+          fixed-tabs
+          next-icon="mdi-arrow-right-bold-box-outline"
+          prev-icon="mdi-arrow-left-bold-box-outline"
+          show-arrows
+          v-model="tab"
         >
-          <v-carousel
-            cycle
-            height="400"
-            hide-delimiter-background
-            show-arrows-on-hover
+          <template v-for="(design, index) in tabHeaders">
+            <v-tab :key="index">{{ design.name }}
+              <v-icon :key="index">{{ design.icon }}</v-icon>
+            </v-tab>
+          </template>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item
+            v-for="(design, index) in designs"
+            :key="index"
           >
-            <v-carousel-item
-              v-for="(source, i) in design.source"
-              :key="i"
-              :src="source.image"
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              show-arrows-on-hover
             >
-            </v-carousel-item>
-          </v-carousel>
-        </v-tab-item>
-      </v-tabs-items>
-    </v-row>
+              <v-carousel-item
+                v-for="(source, i) in design.source"
+                :key="i"
+                :src="source.image"
+              >
+              </v-carousel-item>
+            </v-carousel>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-row>
+    </v-sheet>
+
   </v-container>
 </template>
 <script>
