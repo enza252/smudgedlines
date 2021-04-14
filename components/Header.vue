71caf0ae07a98d3cbbdd94dd9ab16cee12c695d4
ipe <ian.ensor-cic-uk@ibm.com>
2021-04-14 21:34:52 +0100

    make the appbar more responsive to mobiles

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container style="min-width: 100%">
-    <v-row>
+    <v-row xs="12" class="d-flex flex-grow-1">
       <v-app-bar
         color="primary"
         style="border-bottom: 1px solid #ffffff !important;"
@@ -10,17 +10,18 @@
             <v-img :src="slLogoPath" max-height="50" max-width="50"/>
           </template>
         </v-app-bar-nav-icon>
-        <v-app-bar-title>
+        <v-app-bar-title v-if="$vuetify.breakpoint.smAndUp">
           <template v-slot:default>
             <h3 style="color: white">Smudged Lines</h3>
           </template>
         </v-app-bar-title>
         <v-spacer></v-spacer>
+        <!--   Mobile Nav Bar just has icons. Could be a burger bar if changing pages in future     -->
         <template v-if="$vuetify.breakpoint.smAndDown">
           <template v-for="(button, index) in navBarButtons">
             <v-tooltip bottom :key="index">
               <template v-slot:activator="{ on, attrs }">
-                <v-btn color="white" class="ma-1" text v-on="on" v-bind="attrs">
+                <v-btn color="white" class="ma-1" text v-on="on" v-bind="attrs" x-small>
                   <v-icon right >{{ button.icon }}</v-icon>
                 </v-btn>
               </template>
@@ -28,6 +29,7 @@
             </v-tooltip>
           </template>
         </template>
+        <!--   NavBar Buttons ToDo implement scroll to section     -->
         <template v-else>
           <template v-for="(button, index) in navBarButtons">
             <v-tooltip bottom :key="index">
@@ -43,7 +45,6 @@
         </template>
       </v-app-bar>
     </v-row>
-
   </v-container>
 </template>
 
@@ -76,6 +77,6 @@ export default {
     log (item) {
       console.log(item)
     }
-  }
+  },
 }
 </script>
