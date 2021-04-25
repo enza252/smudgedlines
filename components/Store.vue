aa1c2fa39f96c51254c16ec1ff1a3c105bb4917c
iane <mail@ianensor.info>
2021-04-25 21:22:01 +0100

    store and logos

--- components/Store.vue
+++ components/Store.vue
@@ -5,17 +5,22 @@
     </v-row>
     <v-row>
       <v-sheet min-width="100%" color="accent" class="pa-6" rounded elevation="10">
-        <v-row>
+        <v-row >
           <v-col>
-            <v-card>
+            <v-card style="max-height: 100%">
               <v-card-title>Depop</v-card-title>
+              <v-card-text class="pl-3">
+                <v-img :src="depopSVG" max-width="300px" max-height="150px"/>
+              </v-card-text>
             </v-card>
           </v-col>
           <v-col>
             <v-card>
               <v-card-title>Smudged Lines Store</v-card-title>
+              <v-card-text>
+                <v-img :src="slLogoColoured" height="100px" width="100px" style="justify-content: center"/>
+              </v-card-text>
             </v-card>
-            <v-img :src="slLogoColouredSVG" max-height="50" max-width="50"/>
           </v-col>
         </v-row>
       </v-sheet>
@@ -31,8 +36,8 @@ export default defineComponent({
   },
   data () {
     return {
-      slLogoColouredSVG: '/svg/sl-colour-500x500.svg'
-    //   slLogoColouredSVG: '/logo/sl-icon-white-200x200.png' static/svg/sl-colour-500x500.svg
+      depopSVG: '/socials/depop_logo.svg',
+      slLogoColoured: '/logo/sl-colour-150x150.png'
     }
   }
 })
