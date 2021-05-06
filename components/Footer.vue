dc8300e85e2f6511f5912256e859afd8ccd0a2b8
iane <mail@ianensor.info>
2021-05-06 23:36:24 +0100

    get the dp logo in line

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,23 +4,27 @@
   >
     <v-container style="min-width: 100%">
       <v-row>
-        <v-col v-for="(social, index) in socials" cols="1" :key="index" class="pl-2 pt-0 pb-0">
-          <v-tooltip top >
-            <template v-slot:activator="{ on, attrs }">
-              <a :href="social.url">
-                <v-img v-if="social.img" :src="social.img" height="24" max-height="24" width="24" v-on="on" v-bind="attrs" :class="social.classes"/>
-                <v-icon v-else v-on="on" v-bind="attrs" >
-                  {{social.icon}}
-                </v-icon>
-              </a>
-            </template>
-            {{social.tooltipText}}
-          </v-tooltip>
+        <v-col cols="5" class="pl-2 pt-0 pb-0">
+          <template v-for="(social, index) in socials">
+            <v-tooltip top :key="index">
+              <template v-slot:activator="{ on, attrs }">
+                <a :href="social.url">
+                  <v-icon  v-if="social.icon" v-on="on" v-bind="attrs" >
+                    {{social.icon}}
+                  </v-icon>
+                  <span v-else class="pl-1 pt-4" >
+                    <v-img :src="social.img" height="24" width="24" v-on="on" v-bind="attrs" :class="social.classes"/>
+                  </span>
+                </a>
+              </template>
+              {{social.tooltipText}}
+            </v-tooltip>
+
+          </template>
 
         </v-col>
         <v-spacer/>
-        <v-col cols="6" style="text-align: end" class="pa-0">
-          <!-- ToDo mobile version of thisq -->
+        <v-col :cols="mobile ? 7 : 6" style="text-align: end" class="pa-0">
           <span v-if="!mobile">&copy; {{ new Date().getFullYear() }} made with <v-icon>mdi-heart</v-icon> by Smudged Lines</span>
           <span v-else>&copy; {{ new Date().getFullYear() }} Smudged Lines</span>
         </v-col>
@@ -59,7 +63,7 @@ export default defineComponent({
       img: '/socials/depop-logo-transparent.png',
       tooltipText: 'Depop',
       url: 'https://www.depop.com/smudgedlines/',
-      classes: pl6 + ' ' + 'mt-0'
+      classes: 'd-inline-flex flex-grow-1'
     }
     ]
   }),
@@ -70,3 +74,7 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="sass">
+
+</style>
