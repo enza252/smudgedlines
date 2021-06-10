c5daa14ea2886fb74035b727dba0049f41a7812e
iane <mail@ianensor.info>
2021-06-10 21:56:59 +0100

    fix bug

--- components/splash.vue
+++ components/splash.vue
@@ -23,10 +23,8 @@ export default defineComponent({
   },
   methods: {
     useParallax () {
-      if (this.isHydrated && this.$vuetify.breakpoint.xsOnly) {
+      if (this.isHydrated && this.$vuetify.breakpoint.smAndDown) {
         return '/designs/purple-haze/purple-haze-pattern-mobile-2-parallax.jpg'
-      } else if (this.isHydrated && this.$vuetify.breakpoint.lgAndUp) {
-        return '/designs/purple-haze/purple-haze-pattern-og.jpg'
       } else {
         return '/designs/purple-haze/purple-haze-pattern-tablet.jpg'
       }
